3d3b30756dc4e06a9f3efca0b87f156e2ce325e5
Thiago Santos <thiago.santos@moneyex.io>
2018-05-06 18:43:36 -0300

    Graph finalized

--- lib/main.dart
+++ lib/main.dart
@@ -34,76 +34,16 @@ class _MyHomePageState extends State<MyHomePage> {
     return new Scaffold(
       body: new Center(
         child: new Container(
-          color: Colors.white,
-          height: 240.0,
-          width: 320.0,
-          child: new Column(
-            children: <Widget>[
-              new Expanded(
-                child: new Column(
-                  children: <Widget>[
-                    new Container(
-                      margin: const EdgeInsets.all(8.0),
-                      child: new Text('200',
-                        style: new TextStyle(
-                          fontWeight: FontWeight.w500
-                        ),
-                      ),
-                    ),
-                    new Expanded(
-                      child: new Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          new Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              new Container(
-                                margin: const EdgeInsets.only(left: 8.0, right: 14.0),
-                                child: new Text('0',
-                                  style: new TextStyle(
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                          new Expanded(
-                            child: new CustomPaint(
-                              painter: new ProductsChart(),
-                            ),
-                          ),
-                          new Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              new Container(
-                                margin: const EdgeInsets.only(left: 12.0, right: 8.0),
-                                child: new Text('400',
-                                  style: new TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w800,
-                                  )
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Text('Pedidos',
-                    style: new TextStyle(
-                      fontSize: 24.0,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+          height: 400.0,
+          width: 450.0,
+          child: new CustomPaint(
+            painter: new ProductsChart(
+              backgroundColor: Color.fromARGB(255, 228, 228, 228),
+              currentValue: 400,
+              labelColor: Color.fromARGB(255, 116, 116, 118),
+              maxValue: 400,
+              minValue: 0,
+            ),
           ),
         ),
       ),
@@ -113,111 +53,76 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class ProductsChart extends CustomPainter {
 
+  final Color backgroundColor;
+  final int currentValue;
+  final Color labelColor;
+  final int maxValue;
+  final int minValue;
+
+  ProductsChart({
+    this.backgroundColor,
+    this.currentValue, 
+    this.labelColor,
+    this.maxValue, 
+    this.minValue
+  });
+
   @override
   void paint(Canvas canvas, Size size) {
 
     // Current value and angle
-    var totalProducts = 260;
-    var maxTotal = totalProducts > 400 ? 400 : totalProducts;
-    var angle = (180 * maxTotal) / 400.0;
+    final int maxTotal       = currentValue > maxValue ? maxValue : currentValue;
+    final double angle       = (180 * maxTotal) / maxValue;
+    final double cx          = size.width * 0.5;
+    final double cy          = size.height;
+    final Offset origin      = _getOffset(0.0, 0.0);
+    final double labelRadius = cx * 0.8;
 
-    // Clip the canvas
-    final Rect canvasDimensions = Rect.fromLTWH(0.0, 0.0, size.width, size.height);
-    //canvas.clipRect(canvasDimensions);
+    // Set the origin to draw
+    canvas.translate(cx, cy);
 
     // Gray Background
     canvas.drawColor(
-      Color.fromARGB(255, 228, 228, 228), 
+      this.backgroundColor, 
       BlendMode.src,
     );
 
-    // Centered bottom count text
-    final ui.ParagraphBuilder paragraphBuilder = new ui.ParagraphBuilder(
-      new ui.ParagraphStyle(
-        fontSize: 26.0,
-        textAlign: TextAlign.center,
-        textDirection: ui.TextDirection.ltr
-      ),
-    )
-      ..pushStyle(new ui.TextStyle(
-        color: const Color.fromARGB(255, 116, 116, 118),
-        fontWeight: FontWeight.w500,
-      ))
-      ..addText(maxTotal.toString());
+    // Black Arc
+    final double blackArcRadius = _blackArc(cx, origin, canvas);
 
-    final ui.Paragraph paragraph = paragraphBuilder.build()
-      ..layout(new ui.ParagraphConstraints(width: 80.0));
+    // Centered label
+    _centeredLabel(maxTotal, 25.0, blackArcRadius, labelColor, origin, canvas);
 
-    var dxParagraph = size.width * 0.5 - (paragraph.width * 0.5);
-    var offsetParagraph = new Offset(dxParagraph, size.height * 0.8);
-    canvas.drawParagraph(paragraph, offsetParagraph);
+    // Progress Arc
+    _progressArc(cx, origin, angle, canvas);
 
-    // Center bottom for all drawing
-    var offsetCenterBottom = new Offset(size.width / 2, size.height);
-
-    // Primary circle drawing
-    var primaryRadius = size.width * 0.47;
-    //var primaryOffset = _getOffset(angle, primaryRadius);
-    var primaryRect = Rect.fromCircle(
-      center: offsetCenterBottom,
-      radius: primaryRadius,
-    );
-    var primaryPaint = new Paint()
-      ..shader = _generateGradient(primaryRadius)
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = primaryRadius * 0.1
-      ..style = PaintingStyle.fill;
-
-    canvas.drawArc(primaryRect, 0.0, math.pi, false, primaryPaint);
-
-    // Second circle drawing
-    var secondRadius = size.width * 0.3;
-    //var secondOffset = _getOffset(angle, secondRadius);
-    var secondRect = Rect.fromCircle(
-      center: offsetCenterBottom,
-      radius: secondRadius,
-    );
-    var secondPaint = new Paint()
-      ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = size.width * 0.02
-      ..style = PaintingStyle.stroke;
-
-    canvas.drawArc(secondRect, math.pi, math.pi, false, secondPaint);
-
-    // Filled circle drawing
-    var filledRadius = size.width * 0.38;
-    //var filledOffset = _getOffset(angle, filledRadius);
-    var filledRect = Rect.fromCircle(
-      center: offsetCenterBottom,
-      radius: filledRadius,
-    );
-    var filledPaint = new Paint()
-      ..shader = _generateGradient(filledRadius)
-      ..strokeWidth = size.width * 0.15
-      ..style = PaintingStyle.stroke;
-
-    var countedSweepAngle = math.pi * angle / 180;
-    canvas.drawArc(filledRect, math.pi, countedSweepAngle, false, filledPaint);
-
-    // Translate the canvas to center bottom
-    canvas.translate(offsetCenterBottom.dx, offsetCenterBottom.dy);
+    // Gradient Arc
+    final double gradientArcRadius = _gradientArc(cx, origin, canvas);
 
     // Triangle pointer
-    var offsetTriangleTop = _getOffset(angle, primaryRadius - 5);
-    var offsetTriangleLeft = _getOffset(angle + 10, secondRadius);
-    var offsetTriangleRight = _getOffset(angle - 10, secondRadius);
+    _progressMarkArc(angle, gradientArcRadius, blackArcRadius, canvas);
 
-    var black = new Paint()
-      ..color = Colors.black;
+    // Draw left label
+    _drawLeftLabel(
+      this.minValue.toString(), 
+      labelRadius, 
+      this.labelColor, 
+      canvas
+    );
 
-    var path = new Path()
-      ..moveTo(offsetTriangleTop.dx, offsetTriangleTop.dy)
-      ..lineTo(offsetTriangleLeft.dx, offsetTriangleLeft.dy)
-      ..lineTo(offsetTriangleRight.dx, offsetTriangleRight.dy)
-      ..close();
+    _drawTopLabel(
+      (this.maxValue * 0.5).toStringAsFixed(0),
+      labelRadius, 
+      this.labelColor, 
+      canvas
+    );
 
-    canvas.drawPath(path, black);
+    _drawRightLabel(
+      this.maxValue.toString(), 
+      labelRadius, 
+      this.labelColor, 
+      canvas
+    );
 
   }
 
@@ -227,37 +132,211 @@ class ProductsChart extends CustomPainter {
   }
 
   Offset _getOffset(double angle, double radius) {
-    var angleAux = angle + 180.0;
-    var radians = math.pi * angleAux / 180.0;
-    var x = radius * math.cos(radians);
-    var y = radius * math.sin(radians);
+    final double radians = math.pi * angle / 180.0;
+    final double x = radius * math.cos(radians) * -1;
+    final double y = radius * math.sin(radians) * -1;
     return new Offset(x, y);
   }
 
-  /*ui.Gradient _generateGradient(double radius) {
-    return new ui.Gradient.radial(
-      _getOffset(45.0, radius), 
-      radius + 180,
-      [
-        Color.fromARGB(255, 237, 49, 37),
-        Color.fromARGB(255, 251, 175, 64),
-      ],
-    );
-  }*/
-
   ui.Gradient _generateGradient(double radius) {
-    final Offset off0 = _getOffset(-90.0, radius);
-    final Offset off180 = _getOffset(-180.0, radius);
-    print("From: $off0");
-    print("To  : $off180");
     return new ui.Gradient.linear(
-      new Offset(0.0, 45.0), 
-      off180, 
+      _getOffset(0.0, radius),
+      _getOffset(180.0, radius),
       [
         Color.fromARGB(255, 237, 49, 37),
         Color.fromARGB(255, 251, 175, 64),
       ],
     );
+  }
+
+  void _centeredLabel(
+    int value, 
+    double fontSize, 
+    double blackArcRadius, 
+    Color textColor, 
+    ui.Offset origin, 
+    ui.Canvas canvas,
+  ) {
+
+    final ui.ParagraphBuilder paragraphBuilder = new ui.ParagraphBuilder(
+      new ui.ParagraphStyle(
+        fontSize: fontSize,
+        textAlign: TextAlign.center,
+        textDirection: ui.TextDirection.ltr
+      ),
+    )
+      ..pushStyle(new ui.TextStyle(
+        color: textColor,
+        fontWeight: FontWeight.w500,
+      ))
+      ..addText(value.toString());
+    
+    final ui.Paragraph paragraph = paragraphBuilder.build()
+      ..layout(new ui.ParagraphConstraints(width: 80.0));
+
+    if (paragraph.height > blackArcRadius) {
+      _centeredLabel(
+        value,
+        fontSize - 5.0,
+        blackArcRadius, 
+        textColor, 
+        origin, 
+        canvas,
+      );
+      return;
+    }
+    
+    final double dxParagraph = origin.dx - paragraph.width * 0.5;
+    final double dyParagraph = origin.dy - paragraph.height;
+    final Offset offsetParagraph = new Offset(dxParagraph, dyParagraph);
+    canvas.drawParagraph(paragraph, offsetParagraph);
+  }
+
+  double _blackArc(double cx, ui.Offset origin, ui.Canvas canvas) {
+    final double blackArcRadius = cx * 0.5;
+    final Rect blackArcRect = Rect.fromCircle(
+      center: origin,
+      radius: blackArcRadius,
+    );
+    final Paint blackArcPaint = new Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.butt
+      ..strokeWidth = cx * 0.02
+      ..style = PaintingStyle.stroke;
+    
+    canvas.drawArc(blackArcRect, math.pi, math.pi, false, blackArcPaint);
+
+    return blackArcRadius;
+  }
+
+  double _gradientArc(double cx, ui.Offset origin, ui.Canvas canvas) {
+    final double gradientArcRadius = cx * 0.7;
+    final Rect gradientArcRect = Rect.fromCircle(
+      center: origin,
+      radius: gradientArcRadius,
+    );
+    final Paint gradientArcPaint = new Paint()
+      ..shader = _generateGradient(gradientArcRadius)
+      ..strokeCap = StrokeCap.butt
+      ..strokeWidth = gradientArcRadius * 0.1
+      ..style = PaintingStyle.stroke;
+    
+    canvas.drawArc(gradientArcRect, math.pi, math.pi, false, gradientArcPaint);
+
+    return gradientArcRadius;
+  }
+
+  void _progressArc(double cx, ui.Offset origin, double angle, ui.Canvas canvas) {
+    final double progressArcRadius = cx * 0.6;
+    final Rect progressArcRect = Rect.fromCircle(
+      center: origin,
+      radius: progressArcRadius,
+    );
+    final Paint progressArcPaint = new Paint()
+      ..shader = _generateGradient(progressArcRadius)
+      ..strokeWidth = cx * 0.19
+      ..style = PaintingStyle.stroke;
+    
+    final double progressAngle = math.pi * angle / 180;
+    canvas.drawArc(progressArcRect, math.pi, progressAngle, false, progressArcPaint);
+  }
+
+  void _progressMarkArc(double angle, double gradientArcRadius, double blackArcRadius, ui.Canvas canvas) {
+    double rightIncrease = 10.0;
+    double leftIncrease = 10.0;
+    
+    if (angle > 170) {
+      rightIncrease = 180.0 - angle;
+    } else if (angle < 10) {
+      leftIncrease = angle;
+    }
+    
+    final Offset offsetTriangleTop   = _getOffset(angle, gradientArcRadius - (gradientArcRadius * 0.05));
+    final Offset offsetTriangleRight = _getOffset(angle + rightIncrease, blackArcRadius);
+    final Offset offsetTriangleLeft  = _getOffset(angle - leftIncrease, blackArcRadius);
+    
+    final Paint black = new Paint()
+      ..color = Colors.black;
+    
+    final Path path = new Path()
+      ..moveTo(offsetTriangleTop.dx, offsetTriangleTop.dy)
+      ..lineTo(offsetTriangleRight.dx, offsetTriangleRight.dy)
+      ..lineTo(offsetTriangleLeft.dx, offsetTriangleLeft.dy)
+      ..close();
+    
+    canvas.drawPath(path, black);
+  }
+
+  ui.Paragraph _getParagraph(String text, ui.TextStyle style) {
+    final ui.ParagraphBuilder paragraphBuilder = new ui.ParagraphBuilder(
+      new ui.ParagraphStyle(
+        textAlign: TextAlign.center,
+        textDirection: ui.TextDirection.ltr
+      ),
+    )
+      ..pushStyle(style)
+      ..addText(text);
+    
+    final ui.Paragraph paragraph = paragraphBuilder.build()
+      ..layout(new ui.ParagraphConstraints(width: 40.0));
+
+    return paragraph;
+  }
+
+  void _drawLeftLabel(String text, double labelRadius, Color labelColor, Canvas canvas) {
+
+    final ui.TextStyle label0Style = new ui.TextStyle(
+      color: labelColor,
+    );
+
+    final ui.Paragraph paragraph = _getParagraph(text, label0Style);
+
+    final Offset offsetLabel0 = _getOffset(0.0, labelRadius);
+
+    final double dxParagraph = offsetLabel0.dx - paragraph.width * 0.6;
+    final double dyParagraph = offsetLabel0.dy - paragraph.height;
+    final Offset offsetParagraph = new Offset(dxParagraph, dyParagraph);
+
+    canvas.drawParagraph(paragraph, offsetParagraph);
+
+  }
+
+  void _drawTopLabel(String text, double labelRadius, Color labelColor, Canvas canvas) {
+
+    final ui.TextStyle label200Style = new ui.TextStyle(
+      color: labelColor,
+      fontWeight: FontWeight.w500,
+    );
+
+    final ui.Paragraph paragraph = _getParagraph(text, label200Style);
+
+    final Offset offsetLabel0 = _getOffset(90.0, labelRadius);
+
+    final double dxParagraph = offsetLabel0.dx - paragraph.width * 0.5;
+    final double dyParagraph = offsetLabel0.dy - paragraph.height;
+    final Offset offsetParagraph = new Offset(dxParagraph, dyParagraph);
+
+    canvas.drawParagraph(paragraph, offsetParagraph);
+
+  }
+
+  void _drawRightLabel(String text, double labelRadius, Color labelColor, Canvas canvas) {
+
+    final ui.TextStyle label400Style = new ui.TextStyle(
+      color: labelColor,
+      fontWeight: FontWeight.w800,
+    );
+
+    final ui.Paragraph paragraph = _getParagraph(text, label400Style);
+
+    final Offset offsetLabel0 = _getOffset(180.0, labelRadius);
+
+    final double dxParagraph = offsetLabel0.dx - paragraph.width * 0.3;
+    final double dyParagraph = offsetLabel0.dy - paragraph.height;
+    final Offset offsetParagraph = new Offset(dxParagraph, dyParagraph);
+
+    canvas.drawParagraph(paragraph, offsetParagraph);
+
   }
 
 }
