97ab958cbde55063f9c153898a349816024cf443
Thiago Santos <thiago.santos@moneyex.io>
2018-05-08 00:10:54 -0300

    Added interaction

--- lib/main.dart
+++ lib/main.dart
@@ -28,24 +28,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  double currentValue = 148.0;
+  double maxValue = 400.0;
+  double minValue = 0.0;
+
+  void changeCurrentValue(double newValue) {
+    setState(() {
+      currentValue = newValue;      
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Center(
-        child: new Container(
-          height: 400.0,
-          width: double.infinity,
-          child: new CustomPaint(
-            painter: new ProductsChart(
-              backgroundColor: Color.fromARGB(255, 228, 228, 228),
-              currentValue: 148,
-              labelColor: Color.fromARGB(255, 116, 116, 118),
-              maxValue: 400,
-              minValue: 0,
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            flex: 2,
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Slider(
+                  min: this.minValue,
+                  max: this.maxValue,
+                  value: this.currentValue,
+                  onChanged: (double value) {
+                    changeCurrentValue(value);
+                  },
+                ),
+              ],
             ),
           ),
-        ),
+          new Flexible(
+            flex: 1,
+            child: new Container(
+              height: 400.0,
+              margin: const EdgeInsets.only(bottom: 40.0),
+              width: double.infinity,
+              child: new CustomPaint(
+                painter: new ProductsChart(
+                  backgroundColor: Color.fromARGB(255, 228, 228, 228),
+                  currentValue: this.currentValue.toInt(),
+                  labelColor: Color.fromARGB(255, 116, 116, 118),
+                  maxValue: this.maxValue.toInt(),
+                  minValue: this.minValue.toInt(),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -86,12 +119,6 @@ class ProductsChart extends CustomPainter {
 
     // Set the origin to draw
     canvas.translate(cx, cy);
-
-    // Gray Background
-    canvas.drawColor(
-      this.backgroundColor, 
-      BlendMode.src,
-    );
 
     // Black Arc
     final double blackArcRadius = _blackArc(cx, origin, canvas);
